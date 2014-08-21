10bbfea51174a76343069ab24001c75992a3eca4
Johnny Wu <KarasAya@gmail.com>
2014-08-21 13:23:20 +0800

    add project item

--- src/elements/project-tree.js
+++ src/elements/project-tree.js
@@ -3,14 +3,23 @@
 
         load: function ( path ) {
             AssetDB.walk( path, function ( root, stat ) {
-                var el = document.createElement("div");
+                var itemEL = new ProjectItem();
                 if ( stat.isDirectory() ) {
-                    el.innerHTML = "<b>" + stat.name + "</b>";
+                    itemEL.$.name.innerHTML = "<b>" + stat.name + "</b>";
                 }
                 else {
-                    el.innerHTML = stat.name;
+                    itemEL.$.name.innerHTML = stat.name;
                 }
-                this.$.content.appendChild(el);
+                this.$.content.appendChild(itemEL);
+
+                // var el = document.createElement("div");
+                // if ( stat.isDirectory() ) {
+                //     el.innerHTML = "<b>" + stat.name + "</b>";
+                // }
+                // else {
+                //     el.innerHTML = stat.name;
+                // }
+                // this.$.content.appendChild(el);
             }.bind(this) );
         },
 
