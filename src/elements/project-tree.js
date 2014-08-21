ffbe0d44ff3cc934230059b506f5d3a4ad1c3166
Johnny Wu <KarasAya@gmail.com>
2014-08-21 13:42:44 +0800

    make it tree like

--- src/elements/project-tree.js
+++ src/elements/project-tree.js
@@ -1,17 +1,31 @@
 (function () {
     Polymer('project-tree', {
+        created: function () {
+            this.folderElements = {};
+        },
 
         load: function ( path ) {
             AssetDB.walk( path, function ( root, stat ) {
                 var itemEL = new ProjectItem();
                 if ( stat.isDirectory() ) {
                     itemEL.$.name.innerHTML = "<b>" + stat.name + "</b>";
+
+                    this.folderElements[root+"/"+stat.name] = itemEL;
                 }
                 else {
                     itemEL.$.name.innerHTML = stat.name;
                 }
-                this.$.content.appendChild(itemEL);
 
+                var parentEL = this.folderElements[root];
+                if ( parentEL ) {
+                    parentEL.appendChild(itemEL);
+                }
+                else {
+                    itemEL.style.marginLeft="0px";
+                    this.$.content.appendChild(itemEL);
+                }
+
+                // DELME
                 // var el = document.createElement("div");
                 // if ( stat.isDirectory() ) {
                 //     el.innerHTML = "<b>" + stat.name + "</b>";
