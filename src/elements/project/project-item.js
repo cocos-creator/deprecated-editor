9a478c6bac682f9f740796c79ff903f4fed9f747
G_will <g_will@ieqi.com>
2014-09-05 09:46:32 +0800

    simple implement drag drop import asset name collision check

--- src/elements/project/project-item.js
+++ src/elements/project/project-item.js
@@ -26,6 +26,9 @@
             this.isRoot = false;
 
             this.renaming = false;
+
+            // TODO:
+            this.collisionNodes = [];
         },
 
         domReady: function () {
@@ -99,12 +102,6 @@
             event.stopPropagation();
         },
 
-        dragoverAction: function ( event ) {
-            this.fire('draghover');
-
-            event.stopPropagation();
-        },
-
         foldMousedownAction: function ( event ) {
             this.folded = !this.folded;
 
@@ -119,6 +116,144 @@
                 this.fire('namechanged', { name: this.$.rename.value } );
             }
             event.stopPropagation();
+        },
+
+        dragenterAction: function ( event ) {
+            console.log("this =>", this);
+            console.log("project item drag enter action");
+
+            // debug
+            That = this;
+            
+            var nodes;
+            if(this.isFolder) {
+                nodes = this.childNodes;
+
+            }
+            else {
+                nodes = this.parentElement.childNodes;
+            }
+
+            var nodesLen = nodes.length;
+
+            var files = event.dataTransfer.files;
+            var len = files.length;
+            var i;
+
+            var file;
+            var node;
+
+            for(i = 0; i < len; i++) {
+                file = files[i];
+
+                for(j = 0; j < nodesLen; j++) {
+                    
+                    node = nodes[j];
+
+                    // console.log("node :", node);
+                    // console.log("node.basename :", node.basename);
+                    // console.log("node.extname :", node.extname);
+                    // console.log("fullname", node.basename + '.' + node.extname);
+                    // console.log("filename", file.name);
+
+                    if (!node.isFolder && node.basename + node.extname === file.name) {
+                        console.log("conlision ===> ", file.name);
+
+                        // TODO: 
+                        CEL = node.$;
+                        node.$.name.style.color = "red";
+
+
+                        this.collisionNodes.push(node);
+                        // TODO: 是否需要一个全局量 保存 冲突的 node ，待移出时处理？
+                    }
+
+                }
+            }
+
+
+            // var self = this;
+            // FIRE.getDraggingFiles(event, function (files) {
+            //     //self.fire('import', files);
+            //     console.log("files =>", files);
+
+            //     // TODO: 如果 self 是文件 则探测 parent 文件夹
+
+            //     // TODO: 如果 self 是文件夹则直接探测
+
+            // });
+
+            event.preventDefault();
+            event.stopPropagation();
+        },
+
+        dropAction: function ( event ) {
+            console.log("this =>", this);
+            console.log("project item drop action");
+
+            // debug
+            // console.log(event.dataTransfer.types);
+            // Files = event.dataTransfer.files;
+            // console.log(event.dataTransfer.items.length);
+
+            // var self = this;
+            // FIRE.getDraggingItems(event, function (item, isDir) {
+            //     //self.fire('import', files);
+            //     console.log(item);
+            //     console.log(isDir);
+            // });
+
+
+            // TODO: 考虑当前选中的是否是为 文件夹 ，如果是文件夹泽 导入进去 ，不是，则 导入同级。
+
+            That = this;
+            //var dstDir = AssetDB.rpath(this.basename + "://");
+            
+            var files = event.dataTransfer.files;
+            var items = event.dataTransfer.items;
+            var len = items.length;
+            var item;
+            var file;
+
+            for(i=0; i<len; i++){
+                item = items[i];
+                file = files[i];
+
+                if(item.getAsEntry){  //Standard HTML5 API
+                    item = item.getAsEntry();
+                }else if(item.webkitGetAsEntry){  //WebKit implementation of HTML5 API.
+                    item = item.webkitGetAsEntry();
+                }
+
+                //if (entry.isDirectory) {
+                    console.log("item ==> ", item);
+                    console.log("file ==> ", file);
+                // }
+                // else {
+
+                // }
+                
+
+            }
+
+            // TODO:
+            this.fire('dragend');
+            
+
+            event.preventDefault();
+            event.stopPropagation();
+        },
+
+        dragendAction: function( event ) {
+            console.log("project item drag end action");
+
+            var len = this.collisionNodes.length;
+            var i;
+            for(i = 0; i < len; i++) {
+                this.collisionNodes[i].$.name.style.color = "blue";
+            }
+
+            this.collisionNodes = [];
         },
     });
 })();
