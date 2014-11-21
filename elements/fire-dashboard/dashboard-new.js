6ac819dd1e31790f364c08dcea59d82e33c1e6d6
Johnny Wu <KarasAya@gmail.com>
2014-11-22 02:10:17 +0800

    support create new project in dashboard

--- elements/fire-dashboard/dashboard-new.js
+++ elements/fire-dashboard/dashboard-new.js
@@ -1,12 +1,45 @@
 (function () {
+    var Remote = require('remote');
+    var Ipc = require('ipc');
+    var Path = require('fire-path');
+    var Fs = require('fire-fs');
+
     function _getUserHome() {
         return process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
     }
 
     Polymer({
         ready: function () {
-            this.$.name.value = "New Project";
-            this.$.path.value = _getUserHome();
+            this.name = "New Project";
+            this.path = _getUserHome();
+        },
+
+        browseAction: function ( event ) {
+            event.stopPropagation();
+
+            var dialog = Remote.require('dialog');
+
+            var result = dialog.showOpenDialog ( {
+                title: "Choose a project",
+                properties: [ 'openDirectory', 'createDirectory' ]
+            } );
+
+            if ( result ) {
+                this.path = result[0];
+            }
+        },
+
+        createAction: function ( event ) {
+            event.stopPropagation();
+            
+            if ( this.path === '' || this.name === '' )
+                return;
+
+            var projectPath = Path.join( this.path, this.name );
+            if ( Fs.existsSync(projectPath) && Fs.statSync(projectPath).isDirectory() )
+                return;
+
+            Fire.command( 'dashboard:create-project', projectPath );
         },
     });
 })();
