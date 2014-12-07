9bb95931da7175badf975a7d59df17a27ccdbabb
Jare Guo <jare85@163.com>
2014-12-07 18:24:23 +0800

    checks global variables after engine stopped

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,6 +1,7 @@
 ﻿var Sandbox = (function () {
 
     var stashedScene = null;
+    var savedGlobalVars = null;
 
     function cloneScene (scene) {
         return Fire.instantiate(scene);
@@ -10,28 +11,77 @@
         Fire.FObject._deferredDestroy();    // 预先释放一批对象，方便 GC
     }
 
-    function launchScene (scene) {
-        Fire.Engine._setCurrentScene(scene);
-    }
-
     var Sandbox = function () {};
 
-    // 保存当前场景，然后启动新场景。
+    Sandbox._launchScene = function (scene, onUnloaded) {
+        Fire.Engine._setCurrentScene(scene, onUnloaded);
+    };
+
+    // 保存当前场景
     Sandbox.stashScene = function () {
         purgeMemory();
+        this.saveGlobalVars();
         // backup scene
         stashedScene = cloneScene(Fire.Engine._scene);
         //// clone current scene
         //var shadowScene = cloneScene(Fire.Engine._scene);
         //// switch scene
-        //launchScene(shadowScene);
+        //this._launchScene(shadowScene);
     };
 
     // 销毁当前场景，将之前保存的场景还原
     Sandbox.rewindScene = function () {
         purgeMemory();
-        launchScene(stashedScene);
+        this._launchScene(stashedScene, this.resetGlobalVars.bind(this, true));
         stashedScene = null;
+    };
+
+    /**
+     * Take a snapshot of current global variables
+     */
+    Sandbox.saveGlobalVars = function () {
+        savedGlobalVars = {};
+        var globals = window;
+        for (var key in globals) {
+            if (globals.hasOwnProperty(key)) {
+                savedGlobalVars[key] = globals[key];
+            }
+        }
+    };
+
+    /**
+     * @param {boolean} showWarning
+     */
+    Sandbox.resetGlobalVars = function (showWarning) {
+        var key;
+        var globals = window;
+        for (key in globals) {
+            if (globals.hasOwnProperty(key)) {
+                if (key in savedGlobalVars) {
+                    var lastValue = savedGlobalVars[key];
+                    if (globals[key] !== lastValue) {
+                        if (showWarning) {
+                            Fire.warn('Modified global variable: ' + key);
+                        }
+                        globals[key] = lastValue;
+                    }
+                }
+                else {
+                    if (showWarning) {
+                        Fire.warn('Introduced global variable: ' + key);
+                    }
+                    delete globals[key];
+                }
+            }
+        }
+        for (key in savedGlobalVars) {
+            if ( !(key in globals) ) {
+                if (showWarning) {
+                    Fire.warn('Deleted global variable: ' + key);
+                }
+                globals[key] = savedGlobalVars[key];
+            }
+        }
     };
 
     return Sandbox;
