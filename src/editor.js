5b36601875df39cb682a5178531f07622f5a6fd8
KamiSama <1457434343@qq.com>
2014-12-17 11:43:30 +0800

    simple revert

--- src/editor.js
+++ src/editor.js
@@ -2,7 +2,7 @@
     // skip "?fireID="
     // var fireID = JSON.parse(decodeURIComponent(location.search.substr(8)));
     var fireID = -1;
-
+    
     // format: "?foo=bar&hell=world"
     // skip "?"
     var queryString = decodeURIComponent(location.search.substr(1));
@@ -13,41 +13,41 @@
             fireID = parseInt(pair[1]);
         }
     }
-
+    
     //
     var remote = require('remote');
     var ipc = require('ipc');
-
+    
     //
     Fire.mixin( Fire, {
         // console
-        log: function ( text ) {
-            console.log(text);
+        log: function ( text ) { 
+            console.log(text); 
             Fire.sendToCore('console:log', text);
         },
-        warn: function ( text ) {
-            console.warn(text);
+        warn: function ( text ) { 
+            console.warn(text); 
             Fire.sendToCore('console:warn', text);
         },
-        error: function ( text ) {
-            console.error(text);
+        error: function ( text ) { 
+            console.error(text); 
             Fire.sendToCore('console:error', text);
         },
-        success: function ( text ) {
-            console.log('%c' + text, "color: green");
+        success: function ( text ) { 
+            console.log('%c' + text, "color: green"); 
             Fire.sendToCore('console:success', text);
         },
-        failed: function ( text ) {
-            console.log('%c' + text, "color: red");
+        failed: function ( text ) { 
+            console.log('%c' + text, "color: red"); 
             Fire.sendToCore('console:failed', text);
         },
-        info: function ( text ) {
-            console.log('%c' + text, "color: blue");
+        info: function ( text ) { 
+            console.log('%c' + text, "color: blue"); 
             Fire.sendToCore('console:info', text);
         },
-
+        
         // messages
-
+        
         /**
         * Send message to editor-core, which is so called as main app, or atom shell's browser side.
         * @param {string} message - the message to send
@@ -63,7 +63,7 @@
                 Fire.error('The message must be provided');
             }
         },
-
+        
         /**
         * Broadcast message to all pages.
         * The page is so called as atom shell's web side. Each application window is an independent page and has its own JavaScript context.
@@ -81,7 +81,7 @@
                 Fire.error('The message must be provided');
             }
         },
-
+        
         /**
         * Broadcast message to all pages and editor-core
         * @param {string} message - the message to send
@@ -98,7 +98,7 @@
                 Fire.error('The message must be provided');
             }
         },
-
+        
         command: function () {
             Fire.warn('Fire.command is deprecated, use Fire.sendToCore please.');
             Fire.sendToCore.apply(this, arguments);
@@ -107,7 +107,7 @@
             Fire.warn('Fire.broadcast is deprecated, use Fire.sendToPages please.');
             Fire.sendToPages.apply(this, arguments);
         },
-
+        
         rpc: function ( name ) {
             'use strict';
             if ( typeof name === 'string' ) {
@@ -119,7 +119,7 @@
             }
         },
     });
-
+    
     Fire.observe = function ( target, enabled ) {
         if ( !target.isValid ) {
             return;
@@ -132,7 +132,7 @@
             }
         }
     };
-
+    
     Fire.hintObject = function ( target ) {
         if ( target instanceof Fire.Entity ) {
             Fire.sendToPages('entity:hint', target.id );
@@ -144,7 +144,7 @@
             Fire.sendToPages('asset:hint', target._uuid );
         }
     };
-
+    
     Fire.browseObject = function ( type ) {
         if ( Fire.isChildClassOf( type, Fire.Entity ) ) {
             Fire.warn('TODO: ask johnny how to do this.');
@@ -156,7 +156,7 @@
             var typename = Fire.getClassName(type);
             Fire.sendToCore('window:open', 'quick-assets', 'fire://static/quick-assets.html', {
                 title: "Quick Assets",
-                width: 812,
+                width: 800, 
                 height: 600,
                 show: true,
                 resizable: true,
@@ -164,15 +164,15 @@
             } );
         }
     };
-
+    
     // get remote globals
     Fire.AssetDB = remote.getGlobal( 'AssetDB@' + fireID );
     Fire.MainMenu = remote.getGlobal( 'MainMenu@' + fireID );
-
+    
     //
     Fire.plugins = {}; // TODO: 做成Remote Object，确保全局只有一份?
     Fire.gizmos = {};
-
+    
     // init editor-shares after Fire inited
     Fire.Selection.registerMessages(ipc);
 })();
