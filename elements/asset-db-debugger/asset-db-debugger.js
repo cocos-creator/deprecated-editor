83e5b82e4efb52baa2a3be93c5c0f8009299b2f8
Johnny Wu <KarasAya@gmail.com>
2014-12-20 13:10:29 +0800

    update asset-db-debugger

--- elements/asset-db-debugger/asset-db-debugger.js
+++ elements/asset-db-debugger/asset-db-debugger.js
@@ -4,6 +4,11 @@
         created: function () {
             this.ipc = new Fire.IpcListener();
             this.infoList = [];
+            this._option = -1;
+            this.searchValue = "";
+            this.keyName = "";
+            this.valueName = "";
+            this.watchON = false;
         },
 
         attached: function () {
@@ -30,17 +35,54 @@
             this.$.btnGroup.select(0);
         },
 
+        filter: function ( infoList, searchValue ) {
+            var text = searchValue.toLowerCase();
+            var filterList = [];
+
+            for ( var i = 0; i < this.infoList.length; ++i ) {
+                var info = this.infoList[i];
+                if ( info.key.toLowerCase().indexOf(text) !== -1 ) {
+                    filterList.push(info);
+                    continue;
+                }
+
+                if ( info.value.toLowerCase().indexOf(text) !== -1 ) {
+                    filterList.push(info);
+                    continue;
+                }
+            }
+            return filterList;
+        },
+
         pathUuidAction: function ( event ) {
+            this._option = 'path-uuid';
+            this.keyName = "URL";
+            this.valueName = "UUID";
             Fire.sendToCore('asset-db:debugger:query-path-uuid');
         },
 
         uuidPathAction: function ( event ) {
+            this._option = 'uuid-path';
+            this.keyName = "UUID";
+            this.valueName = "URL";
             Fire.sendToCore('asset-db:debugger:query-uuid-path');
         },
 
         libraryAction: function ( event ) {
+            this._option = 'library';
+            this.keyName = "UUID";
+            this.valueName = "URL";
+            this.infoList = [];
             // TODO
             // Fire.sendToCore('asset-db:debugger:query-path-uuid');
+        },
+
+        refreshAction: function ( event ) {
+            switch (this._option) {
+            case 'path-uuid': this.pathUuidAction(); break;
+            case 'uuid-path': this.uuidPathAction(); break;
+            case 'library': this.libraryAction(); break;
+            }
         },
     });
 })();
