f86430bbd270c34c4cb0f4b6448f4a63427df264
Johnny Wu <KarasAya@gmail.com>
2014-12-22 19:31:05 +0800

    optimize inspector rendering

--- elements/fire-inspector/fire-inspector.js
+++ elements/fire-inspector/fire-inspector.js
@@ -5,8 +5,6 @@
     Polymer({
         created: function () {
             this.target = null;
-            this.isEntity = false;
-            this.isImporter = false;
             this.icon = new Image();
             this.icon.src = "fire://static/img/plugin-inspector.png";
 
@@ -51,7 +49,7 @@
                     // Checks whether last uuid modified to ensure call stack not suspended by another ipc event
                     // This may occurred after ipc sync invocation such as AssetDB.xxx
                     if (meta && this.lastUuid === id) {
-                        // this.inspect 前如果不延迟一帧，点右键菜单时渲染会只进行到一半，同时 inspector 切换也会有问题。
+                        // one frame dely to make sure mouse right click event (contextmenu popup) will not suspend the rendering
                         process.nextTick(function (meta) {
                             // Only inspect the lastest one
                             if (this.lastUuid === id) {
@@ -75,46 +73,104 @@
             }
         },
 
+        _updateEntityToolbar: function () {
+            var toolbar = this.$.toolbar;
+            var target = this.target;
+            var el = new FireCheckbox();
+            el.bind('value', new PathObserver( target, 'active'));
+            toolbar.appendChild(el);
+
+            el = new FireTextInput();
+            el.bind('value', new PathObserver( target, 'name'));
+            el.setAttribute('flex-1','');
+            toolbar.appendChild(el);
+
+            el = document.createElement('i');
+            el.id = 'btnAddComp';
+            el.classList.add('fa', 'fa-plus');
+            el.addEventListener('click', this.addComponentAction.bind(this) );
+            toolbar.appendChild(el);
+        },
+
+        _updateImporterToolbar: function () {
+            var toolbar = this.$.toolbar;
+            var target = this.target;
+
+            toolbar.setAttribute('justify-end','');
+
+            var el = new FireButton();
+            var observer = new PathObserver(target, 'dirty');
+            var transformObserver = new ObserverTransform(observer, function ( value ) {
+                return !value;
+            });
+            el.bind('disabled', transformObserver );
+            el.addEventListener('click', this.revertAction.bind(this));
+            var icon = document.createElement('i');
+            icon.classList.add('fa', 'fa-close');
+            el.appendChild(icon);
+            toolbar.appendChild(el);
+
+            el = new FireButton();
+            observer = new PathObserver(target, 'dirty');
+            transformObserver = new ObserverTransform(observer, function ( value ) {
+                return !value;
+            });
+            el.bind('disabled', transformObserver );
+            el.addEventListener('click', this.applyAction.bind(this));
+            icon = document.createElement('i');
+            icon.classList.add('fa', 'fa-check');
+            el.appendChild(icon);
+            toolbar.appendChild(el);
+
+        },
+
         inspect: function ( obj, force ) {
             //
             if ( !force ) {
                 //
-                if ( this.$.fields.target === obj ) {
+                if ( this.target === obj ) {
                     return;
                 }
 
                 //
-                if ( this.$.fields.target instanceof Fire.Importer &&
+                if ( this.target instanceof Fire.Importer &&
                      obj instanceof Fire.Importer )
                 {
-                    if ( this.$.fields.target.uuid === obj.uuid ) {
+                    if ( this.target.uuid === obj.uuid ) {
                         return;
                     }
                 }
             }
 
             //
-            this.target = obj;
-            this.isEntity = obj instanceof Fire.Entity;
-            this.isImporter = obj instanceof Fire.Importer;
-
-            //
-            if ( this.$.preview.firstChild ) {
-                this.$.preview.removeChild(this.$.preview.firstChild);
-            }
-            this.$.preview.setAttribute('hidden','');
-
-            //
-            if ( this.$.fields.target ) {
-                Fire.observe(this.$.fields.target,false);
+            if ( this.target ) {
+                Fire.observe(this.target,false);
             }
             if ( obj ) {
                 Fire.observe(obj,true);
             }
+            this.target = obj;
             this.$.fields.target = obj;
+
+            // update toolbar
+            while (this.$.toolbar.firstElementChild) {
+                this.$.toolbar.removeChild(this.$.toolbar.firstElementChild);
+            }
+            this.$.toolbar.removeAttribute('justify-end');
+            if ( obj instanceof Fire.Entity ) {
+                this._updateEntityToolbar();
+            }
+            else if ( obj instanceof Fire.Importer ) {
+                this._updateImporterToolbar();
+            }
+
+            // update fileds
             this.$.fields.refresh();
 
-            //
+            // update preview
+            if ( this.$.preview.firstElementChild ) {
+                this.$.preview.removeChild(this.$.preview.firstElementChild);
+            }
             if ( obj instanceof Fire.TextureImporter ) {
                 var img = new Image();
                 img.src = "uuid://" + obj.uuid;
@@ -123,6 +179,9 @@
                 div.appendChild(img);
                 this.$.preview.appendChild(div);
                 this.$.preview.removeAttribute('hidden');
+            }
+            else {
+                this.$.preview.setAttribute('hidden','');
             }
         },
 
