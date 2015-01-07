e9e367ec730d8f8fb4e50b9031a1b38d5d5873e6
Johnny Wu <KarasAya@gmail.com>
2015-01-08 00:04:39 +0800

    add watching status

--- elements/main-window/status-bar/status-bar.js
+++ elements/main-window/status-bar/status-bar.js
@@ -1,7 +1,36 @@
 var remote = require('remote');
+var assetDBStates = {
+    "start-watching": { text: "Start Watching" },
+    "watching": { text: "Watching" },
+    "stop-watching": { text: "Stop Watching" },
+    "syncing": { text: "Syncing" },
+    "normal": { text: "Normal" },
+    "unknown": { text: "Unknown" },
+};
 
 Polymer({
     created: function () {
+        this.ipc = new Fire.IpcListener();
+
         this.version = remote.getGlobal( 'FIRE_VER' );
+        this.dbState = "normal";
+        this.dbStateText = assetDBStates[this.dbState].text;
+    },
+
+    attached: function () {
+        this.ipc.on('asset-db:status-changed', function ( status ) {
+            var state = assetDBStates[status];
+            if ( !state ) {
+                status = "unknown";
+                state = assetDBStates.unknown;
+            }
+
+            this.dbState = status;
+            this.dbStateText = state.text;
+        }.bind(this) );
+    },
+
+    detached: function () {
+        this.ipc.clear();
     },
 });
