ba88ae630da73aa6ebdeec10b2c86e6280ccaf93
KamiSama <1457434343@qq.com>
2015-01-15 19:14:20 +0800

    增加搜索 keymap切换 主题切换 保存 搜索 和其他样式修改

--- elements/code-editor/code-mirror.js
+++ elements/code-editor/code-mirror.js
@@ -1,15 +1,35 @@
+var Fs = require("fire-fs");
+
 Polymer({
     value: '',
     mode: 'javascript',
-    theme: 'solarized dark',
+    theme: 'zenburn',
     tabSize: 4,
+    keyMap: 'sublime',
     lineNumbers: true,
+    owner: null,
+    filePath: "",
 
     refresh: function() {
         this.mirror.refresh();
     },
 
     ready: function () {
+    },
+
+    domReady: function () {
+    },
+
+    valueChanged: function() {
+        if (this.mirror !== undefined){
+            this.mirror.setValue(this.value);
+        }
+        else {
+            this.createEditor();
+        }
+    },
+
+    createEditor: function () {
         this.mirror = CodeMirror(this.shadowRoot, {
             value: this.value,
             mode: this.mode,
@@ -20,27 +40,25 @@ Polymer({
             foldGutter: true,
             matchBrackets: true,
             styleActiveLine: true,
+            autoCloseBrackets: true,
+            showCursorWhenSelecting: true,
+            keyMap: this.keyMap,
             gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
         });
 
-
         this.mirror.on('focus',function () {
-            console.log('focus');
         });
-    },
 
-    domReady: function () {
         this.mirror.on('change',function () {
-            this.updateLineCount();
+        }.bind(this));
+
+        this.mirror.on('cursorActivity',function () {
+            this.owner.Cursor = this.mirror.getCursor();
         }.bind(this));
     },
 
-    valueChanged: function() {
-        this.mirror.setValue(this.value);
-    },
-
-    updateLineCount: function () {
-        console.log(this.mirror.lineCount());
+    keyMapChanged: function () {
+        this.mirror.setOption('keyMap', this.keyMap);
     },
 
     modeChanged: function() {
@@ -49,6 +67,11 @@ Polymer({
 
     themeChanged: function() {
         this.mirror.setOption('theme', this.theme);
+    },
+
+    //NOTE: 统一修改右下角通知
+    tipToast: function (text) {
+        this.owner.$.tip.innerHTML = text;
     },
 
     tabSizeChanged: function() {
@@ -62,4 +85,30 @@ Polymer({
     focus: function() {
         this.mirror.focus();
     },
+
+    //NOTE:comment your select line(注释选中行)
+    lineComment: function () {
+        var range = { from: this.mirror.getCursor(true), to: this.mirror.getCursor(false) };
+        this.mirror.lineComment(range.from, range.to);
+    },
+
+    //NOTE: auto format
+    autoFormat: function () {
+        var range = { from: this.mirror.getCursor(true), to: this.mirror.getCursor(false) };
+        this.mirror.autoFormatRange(range.from, range.to);
+    },
+
+    //NOTE: Save to file
+    saveModifed: function () {
+        Fs.writeFile(this.filePath,this.mirror.getValue(), 'utf8', function ( err ) {
+            if (!err){
+                this.tipToast("save success!");
+            }
+        }.bind(this));
+    },
+
+    themeChanged: function () {
+        this.mirror.setOption('theme', this.theme);
+    },
+
 });
