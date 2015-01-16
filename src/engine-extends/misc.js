e49950c97bccfa9d1d5b13795794303fdbd38b4d
Jare Guo <jare85@163.com>
2015-01-16 15:52:05 +0800

    add Fire.$0 for debugging

--- src/engine-extends/misc.js
+++ src/engine-extends/misc.js
@@ -5,3 +5,12 @@ Fire._idToObject = idToObject;
 Fire._getInstanceById = function (id) {
     return idToObject[id];
 };
+
+Object.defineProperty(Fire, '$0', {
+    get: function () {
+        var id = Fire.Selection.entities[0];
+        if (id) {
+            return Fire._getInstanceById(id);
+        }
+    }
+});
