045436fc208ae0d22be0f94908a0d820e09fbf88
KamiSama <1457434343@qq.com>
2015-01-29 16:33:11 +0800

    样式修改

--- elements/fire-console/fire-console.js
+++ elements/fire-console/fire-console.js
@@ -105,7 +105,7 @@ Polymer({
         }
 
         filterLogs = filterLogs.map(function (item) {
-            return {type:item.type,text:item.text,count:0}
+            return {type:item.type,text:item.text,count:0};
         });
 
         if ( this.collapse && filterLogs.length > 0 ) {
