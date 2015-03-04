88740b1d89d96ee63633aefe12cbeb422c8c68e7
Johnny Wu <KarasAya@gmail.com>
2015-03-04 12:53:34 +0800

    fix fireball-x/dev#379, use on-click instead of mouse-up when selecting

--- elements/fire-hierarchy/hierarchy-item.js
+++ elements/fire-hierarchy/hierarchy-item.js
@@ -31,51 +31,6 @@ Polymer({
         this.renaming = false;
     },
 
-    mousedownAction: function ( event ) {
-        this.super([event]);
-        if (event.cancelBubble) {
-            return;
-        }
-        var mouseLeft = event.which === 1;
-        if (mouseLeft) {
-            // selecting
-            this._isToggle = false;
-            this._isShift = false;
-
-            if ( event.shiftKey ) {
-                this._isShift = true;
-            }
-            else if ( event.metaKey || event.ctrlKey ) {
-                this._isToggle = true;
-            }
-
-            this.fire('selecting', {
-                toggle: this._isToggle,
-                shift: this._isShift,
-                x: event.x,
-                y: event.y,
-            } );
-
-            event.stopPropagation();
-        }
-    },
-
-    mouseupAction: function ( event ) {
-        this.super([event]);
-        if (event.cancelBubble) {
-            return;
-        }
-        var mouseLeft = event.which === 1;
-        if (mouseLeft) {
-            this.fire('select', {
-                toggle: this._isToggle,
-                shift: this._isShift
-            } );
-
-            event.stopPropagation();
-        }
-    },
-
     mouseenterAction: function ( event ) {
         Fire.Selection.hoverEntity(this.userId);
         event.stopPropagation();
