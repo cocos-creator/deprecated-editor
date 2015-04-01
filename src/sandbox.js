1618e5d8ae7344f386f44289bada928f264f936b
Jare Guo <jare85@163.com>
2015-04-01 12:29:05 +0800

    ignore "mixpanel" in global variable's check

--- src/sandbox.js
+++ src/sandbox.js
@@ -331,7 +331,7 @@ Sandbox.reloadScripts = (function () {
         Fire.LoadManager.reset();
         // 清除 browserify 声明的 require 后，除非用户另外找地方存了原来的 require，否则之前的脚本都将会被垃圾回收
         require = Sandbox.nodeJsRequire;
-        Sandbox.globalVarsChecker.restore(Fire.log, 'purging', 'require');
+        Sandbox.globalVarsChecker.restore(Fire.log, 'purging', 'require', 'mixpanel');
         Fire._requiringStack = [];
     }
 
