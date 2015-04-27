ebc125ac1956dd0ab4e09561337da69c19f39d24
kamisama <1457434343@qq.com>
2015-04-27 16:34:49 +0800

    update fireball-x/dev#615  用户登录后管理的 popup 窗口

--- elements/main-window/top-bar/popup/pop-up.js
+++ elements/main-window/top-bar/popup/pop-up.js
@@ -1,3 +1,18 @@
 Polymer({
+    publish: {
+        userObj: null,
+    },
 
+    AccountManage: function () {
+        var shell = require('shell');
+        shell.openExternal('http://fireball-x.com/user/edit');
+        this.style.display = 'none';
+    },
+
+    loginOut: function () {
+        this.style.display = 'none';
+        Editor.logout(function (){
+            console.log('log out');
+        });
+    },
 });
--- elements/main-window/top-bar/top-bar.js
+++ elements/main-window/top-bar/top-bar.js
@@ -1,5 +1,6 @@
 Polymer({
     keySettings: null,
+    popUp: null,
 
     helpAction: function () {
         if (!this.keySettings) {
@@ -12,7 +13,20 @@ Polymer({
 
     },
 
-    loginAction: function () {
-        // Editor.sendToCore('fire-login:open');
+    profile: function () {
+        if (!this.popUp) {
+            this.popUp = new PopUp();
+            this.popUp.userObj = Editor.userInfo;
+            document.body.appendChild(this.popUp);
+        }
+        else {
+            if (this.popUp.style.display !== 'none') {
+                this.popUp.style.display = 'none';
+            }
+            else {
+                this.popUp.style.display = 'block';
+            }
+        }
+
     },
 });
