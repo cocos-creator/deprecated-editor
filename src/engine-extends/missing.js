f5474fa194080ad4b5d0cca53004abbffc0ec7f0
Jare Guo <jare85@163.com>
2015-05-30 12:20:19 +0800

    API: internal codes should not use yuidoc comment blocks otherwise yuidoc will output many warnings (fireball-x/dev#693)

--- src/engine-extends/missing.js
+++ src/engine-extends/missing.js
@@ -43,10 +43,10 @@ MissingScript.get('errorInfo',
 // NOTE: '_$erialized' 这个特殊标记只有声明为最后一个 prop 才有用，这个FireClass的原始序列化数据将直接赋给 prop。
 MissingScript.prop('_$erialized', null, Fire.HideInInspector, Fire.EditorOnly);
 
-/**
- * @param {string} id
- * @return {function} constructor
- */
+///**
+// * @param {string} id
+// * @return {function} constructor
+// */
 MissingScript.safeFindClass = function (id) {
     var cls = Fire.JS._getClassById(id);
     if (cls) {
